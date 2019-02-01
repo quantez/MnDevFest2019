ec866d4988e93fda5283623cfcd8a706c0be4de0
Steve Peterson <quantez@gmail.com>
2019-01-31 22:40:05 -0600

    Clean up and formatting

--- app/src/main/java/com/aandtech/navcomponent/MainActivity.kt
+++ app/src/main/java/com/aandtech/navcomponent/MainActivity.kt
@@ -9,28 +9,11 @@ import androidx.navigation.ui.NavigationUI.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-//            R.id.navigation_home -> {
-//                message.setText(R.string.title_home)
-//                return@OnNavigationItemSelectedListener true
-//            }
-//            R.id.navigation_dashboard -> {
-//                message.setText(R.string.title_dashboard)
-//                return@OnNavigationItemSelectedListener true
-//            }
-//            R.id.navigation_notifications -> {
-//                message.setText(R.string.title_notifications)
-//                return@OnNavigationItemSelectedListener true
-//            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val navController = findNavController(this, R.id.nav_host_fragment)
+        setupWithNavController(navigation, navController)
     }
 }
