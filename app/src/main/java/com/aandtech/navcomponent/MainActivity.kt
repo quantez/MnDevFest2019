a90bbc3fd745df38fdbdf7e55f67bf1af4abcb47
Steve Peterson <quantez@gmail.com>
2019-02-01 11:48:18 -0600

    Add Toolbar

--- app/src/main/java/com/aandtech/navcomponent/MainActivity.kt
+++ app/src/main/java/com/aandtech/navcomponent/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.aandtech.navcomponent
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 
@@ -14,6 +16,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(this, R.id.nav_host_fragment)
-        setupWithNavController(navigation, navController)
+        setupWithNavController(bottomNavigation, navController)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    // TODO way to get the up button to work with Nav Component.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
